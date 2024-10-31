7f4f356b58516a313b6505f04257384d7cf56851
Haezebrouck Jules <jules.haezebrouck@uzgent.be>
2024-10-31 13:34:27 +0100

    re-add deprecated attribute

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,8 @@ terraform {
 provider "azurerm" {
   features {}
 
+  skip_provider_registration = true
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   tenant_id       = var.tenant_id
