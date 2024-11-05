31c4bbd6d7ca163c9c2904aadc82d1bc2d313cd5
Haezebrouck Jules <jules.haezebrouck@uzgent.be>
2024-11-05 11:24:06 +0100

    add outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "nomad_address" {
+  value = "http://${azurerm_linux_virtual_machine.server[0].private_ip_address}:"
+}
+
+output "consul_bootstrap_token_secret" {
+  value = random_uuid.nomad_consul_token_secret.result
+}
+
+output "consul_address" {
+  value = "http://${azurerm_linux_virtual_machine.server[0].private_ip_address}:8500"
+}
+
+output "job_filesystem_name" {
+  value = azurerm_storage_share.hashistack_job_filesystem.name
+}
