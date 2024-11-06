f46ee2d4d5255c8516895d4b193d7b340e73bf95
Haezebrouck Jules <jules.haezebrouck@uzgent.be>
2024-11-06 10:59:59 +0100

    test output 13

---
+++ terraform/data-scripts/test-scripting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+touch /script_has_worked.txt
+
+
+echo "================================"
+echo "==                            =="
+echo "==                            =="
+echo "==            OK              =="
+echo "==                            =="
+echo "==                            =="
+echo "================================"
