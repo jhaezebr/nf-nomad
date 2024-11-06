f75de2aa79c74a8841abe5403009fdd3868e30ac
Haezebrouck Jules <jules.haezebrouck@uzgent.be>
2024-11-06 17:10:18 +0100

    move acl variable to scripts

--- terraform/data-scripts/user-data-client.sh
+++ terraform/data-scripts/user-data-client.sh
@@ -3,14 +3,20 @@
 set -xe
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo bash /ops/shared/scripts/client.sh "${cloud_env}" '${retry_join}' "${nomad_binary}"
+
+cat <<-'EOF' > /tmp/client.env
+  CLOUD=${cloud_env}
+  RETRY_JOIN=${retry_join}
+  NOMAD_BINARY=${nomad_binary}
+  CONSUL_ACL_ENABLED=${consul_acl_enabled}
+  NOMAD_ACL_ENABLED=${nomad_acl_enabled}
+EOF
+
+sudo bash /ops/shared/scripts/client.sh /tmp/client.env
 
 CLOUD_ENV="${cloud_env}"
 
 sed -i "s/CONSUL_TOKEN/${nomad_consul_token_secret}/g" /etc/nomad.d/nomad.hcl
-sed -i "s/NOMAD_ACL_ENABLED/${nomad_acl_enabled}/g" /etc/nomad.d/nomad.hcl
-
-sed -i "s/CONSUL_ACL_ENABLED/${consul_acl_enabled}/g" /etc/consul.d/consul.hcl
 
 
 case $CLOUD_ENV in
--- terraform/data-scripts/user-data-server.sh
+++ terraform/data-scripts/user-data-server.sh
@@ -5,13 +5,21 @@ source /ops/shared/data-scripts/subroutines/nomad-bootstrap.sh
 set -xe
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo bash /ops/shared/scripts/server.sh "${cloud_env}" "${server_count}" '${retry_join}' "${nomad_binary}"
+
+cat <<-'EOF' > /tmp/server.env
+  CLOUD=${cloud_env}
+  SERVER_COUNT=${server_count}
+  RETRY_JOIN=${retry_join}
+  NOMAD_BINARY=${nomad_binary}
+  CONSUL_ACL_ENABLED=${consul_acl_enabled}
+  NOMAD_ACL_ENABLED=${nomad_acl_enabled}
+EOF
+
+
+sudo bash /ops/shared/scripts/server.sh /tmp/server.env
 
 
 sed -i "s/CONSUL_TOKEN/${nomad_consul_token_secret}/g" /etc/nomad.d/nomad.hcl
-sed -i "s/NOMAD_ACL_ENABLED/${nomad_acl_enabled}/g" /etc/nomad.d/nomad.hcl
-
-sed -i "s/CONSUL_ACL_ENABLED/${consul_acl_enabled}/g" /etc/consul.d/consul.hcl
 
 sudo systemctl restart consul
 sudo systemctl restart nomad
